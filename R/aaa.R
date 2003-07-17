3ee0ec967100b2534cb3c6e6512ab0f88561e7f6
Martin Maechler <maechler@stat.math.ethz.ch>
2003-07-17 00:00:00 +0000

    version 0.8-3

---
+++ R/aaa.R
@@ -0,0 +1,16 @@
+## running in R or S+ ? -- the following even works in S(not plus):
+if(!(Ex <- exists("version")) || is.null(vl <- version$language) || vl != "R")
+{ ## we are not in R :
+    is.R <- function () FALSE
+    if(!Ex || version$major < 6)
+        "%in%" <- function(x, table) !is.na(match(x, table, nomatch = NA))
+
+    dev.interactive <- function () {
+	interactive() && exists(".Device") &&
+	.Device %in% c("motif", "X11", "openlook", "graphsheet")
+    }
+
+    which.min <- function(x) sort.list(x)[1]
+}
+if(Ex) rm(vl)
+rm(Ex)
